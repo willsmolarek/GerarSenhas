f133ebf5278edaeec53e6a2db962c926d47048b5
William de Camargo Smolarek <111304963+willsmolarek@users.noreply.github.com>
2023-04-03 20:31:54 -0300

    Update senhas.py

--- senhas.py
+++ senhas.py
@@ -1,12 +1,10 @@
 import random
 import string
 
-# Define as opções de caracteres para cada nível de complexidade
 complexidade_baixa = string.ascii_lowercase
 complexidade_media = string.ascii_letters + string.digits
 complexidade_alta = string.ascii_letters + string.digits + string.punctuation
 
-# Define a função que gera a senha aleatória
 def gerar_senha(tamanho, complexidade):
     caracteres = ""
     if complexidade == "baixa":
@@ -21,10 +19,8 @@ def gerar_senha(tamanho, complexidade):
     senha = "".join(random.choice(caracteres) for i in range(tamanho))
     return senha
 
-# Pede ao usuário o tamanho e a complexidade da senha
 tamanho = int(input("Qual o tamanho da senha? "))
 complexidade = input("Qual a complexidade da senha (baixa, media ou alta)? ")
 
-# Gera a senha e imprime na tela
 senha = gerar_senha(tamanho, complexidade)
 print("Sua senha é:", senha)
